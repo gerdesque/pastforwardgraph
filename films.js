091a2f4e06b83636745e5ebeae242a0be4ce9635
Gerd Müller <gerdesk@posteo.org>
2018-11-11 20:36:36 +0100

    first commit

---
+++ films.js
@@ -0,0 +1,116 @@
+{  
+  "nodes":[
+  {
+    "name": "European War, 1914-1915. The African Zouaves in the Flanders",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/71579_13050_The_african_zouaves_DSM_file_to_view.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/71579_13050_The_african_zouaves_DSM_file_to_view.mp4",
+    "start": "0:00",
+    "end": "0:30",
+    "association1": "Text1",
+    "association2": "Text2"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_1.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_1.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_2.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_2.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_3.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_31_13_1R_Wochenschau_Material_1897_1913_Teil_3.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_50_145_1R_Kino_Kriegsschau_Nr_14_(DE_1914).jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_50_145_1R_Kino_Kriegsschau_Nr_14_DE_1914.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_50_300_1R_Bilder_vom_Trojanischen_Kriege_(Helena).jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_50_300_1R_Bilder_vom_Trojanischen_Kriege_(Helena).mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_50_341_1R_unident%20Eiko-Woche%20-%20Fragment.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_50_341_1R_unident%20Eiko-Woche%20-%20Fragment.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_BArch_K_214956_1R_Hueter_der_Ordnung_LaengunguGradingZT.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_BArch_K_214956_1R_Hueter_der_Ordnung_LaengunguGradingZT.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/EFG1914-00013.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/Vor%20dem%20Damenbad.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/EFG1914-00017.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/Willst%20du%20mein%20Cousinchen%20sein.mp4"
+  },
+  {
+    "name": "Beim Zahnarzt",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/EFG1914-00025.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/Beim%20Zahnarzt.mp4"
+  },
+  {
+    "name": "Der Ertrag der VI. deutschen Kriegsanleihe gibt unseren Helden die Kraft zum Siege",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_1_02_16_182_1R_Der_Ertrag_der_VI_deutschen_Kriegsanleihen_gibt_unseren_Helden_die_Kraft_zum_Siege.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_1_02_16_182_1R_Der_Ertrag_der_VI_deutschen_Kriegsanleihen_gibt_unseren_Helden_die_Kraft_zum_Siege.mp4"
+  },
+  {
+    "name": "Hindenburgs 70. Geburtstag im Großen Hauptquartier",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_1_02_35_255_1R_Hindenburgs_70_Geburtstagsfeier_beim_Kaiser_im_grossen_Hauptquartier.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_1_02_35_255_1R_Hindenburgs_70_Geburtstagsfeier_beim_Kaiser_im_grossen_Hauptquartier.mp4"
+  },
+  {
+    "name": "Vater ist im Kriege",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_Vater%20ist%20im%20Kriege.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_Vater%20ist%20im%20Kriege.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_Auf%20dem%20Kasernenhof.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_Auf%20dem%20Kasernenhof.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_Rueckkehr%20von%20der%20Parade.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_Rueckkehr%20von%20der%20Parade.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_Wintersport%20in%20Oesterreich.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_Wintersport%20in%20Oesterreich.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_10_1194_2R_Tirol_in_Waffen_(Le_Tyrol_en_Armes).jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_10_1194_2R_Tirol_in_Waffen_(Le_Tyrol_en_Armes).mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_11_306_1R_Oorlogsannalen_No_85_(1918)_orange.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_11_306_1R_Oorlogsannalen_No_85_(1918)_orange.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_20_292_1R_William_Held_Film%20-%20Hunger_Blockade_Germany.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_20_292_1R_William_Held_Film%20-%20Hunger_Blockade_Germany.mp4"
+  },
+  {
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_20_306_1R_Das_Begraebnis_der_ehemaligen_Kaiserin_Auguste_Victoria.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_20_306_1R_Das_Begraebnis_der_ehemaligen_Kaiserin_Auguste_Victoria.mp4"
+  }
+],
+"arcs":[
+  {
+      "source": "European War, 1914-1915. The African Zouaves in the Flanders",
+      "target": "Beim Zahnarzt"
+  },
+  {
+      "source": "European War, 1914-1915. The African Zouaves in the Flanders",
+      "target": "Der Ertrag der VI. deutschen Kriegsanleihe gibt unseren Helden die Kraft zum Siege"
+  },
+  {
+      "source": "Beim Zahnarzt",
+      "target": "Hindenburgs 70. Geburtstag im Großen Hauptquartier"
+  },
+  {
+      "source": "Beim Zahnarzt",
+      "target": "Vater ist im Kriege"
+  }
+  
+]
+}
---
+++ index.js
@@ -0,0 +1,82 @@
+var width = window.innerWidth,
+height = window.innerHeight;
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .size([width, height])
+    .linkDistance(width/3.05)
+    .charge(-500)
+    .start();
+
+var svg = d3.select("#main-content").append("svg")
+            .attr("width", width)
+            .attr("height", height);   
+
+d3.json("films.js", function(error, graph){ 
+   
+var nodes = {};
+var links = graph.arcs;
+  
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+  
+});
+  force
+      .nodes(d3.values(nodes))
+      .links(links)
+      .on("tick", tick)
+      .start();
+
+  svg.append("defs").selectAll("marker")
+    .data(0)
+  .enter().append("marker")
+    .attr("id", function(d) { return d; })
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 25)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+var path = svg.append("g").selectAll("path")
+    .data(force.links())
+  .enter().append("path")
+    .attr("class", function(d) { return "link " + 0; });
+
+var circle = svg.append("g").selectAll("circle")
+    .data(force.nodes())
+  .enter().append("circle")
+    .attr("r", 25)
+    .call(force.drag); 
+
+var text = svg.append("g").selectAll("text")
+    .data(force.nodes())
+  .enter().append("text")
+    .attr("x", 25)
+    .attr("y", ".31em")
+    .text(function(d) { return d.name; });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+  path.attr("d", linkArc);
+  circle.attr("transform", transform);
+  text.attr("transform", transform);
+}
+
+function linkArc(d) {
+  var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y,
+      dr = Math.sqrt(dx * dx + dy * dy);
+  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+}
+
+function transform(d) {
+  return "translate(" + d.x + "," + d.y + ")";
+}
+
+
+});
