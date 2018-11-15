176c90c20e63b56797fe4f95f7fd78566e71fbe8
Jana Hentschke <jentschk@users.noreply.github.com>
2018-11-15 22:28:12 +0100

    Update films.js
    Sections #2 und #3 ergänzt und eine erste Verknüpfung dazwischen

--- films.js
+++ films.js
@@ -12,12 +12,43 @@
     "associationText2": "Teaser der zweiten Assoziation",
     "text": "Kaiser Wilhelm beim Entenfüttern"
 
+  },
+      {
+    "id": 2,
+    "section": "Der Ertrag der VI. deutschen Kriegsanleihe gibt unseren Helden die Kraft zum Siege",
+    "thumbnail": "https://www.filmportal.de/sites/default/files/video_thumb/DIF_1_02_16_182_1R_Der_Ertrag_der_VI_deutschen_Kriegsanleihen_gibt_unseren_Helden_die_Kraft_zum_Siege.jpg",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_1_02_16_182_1R_Der_Ertrag_der_VI_deutschen_Kriegsanleihen_gibt_unseren_Helden_die_Kraft_zum_Siege.mp4",
+    "start": "0:00",
+    "end": "1:25",
+    "intro": "Propaganda vor dem Kino-Hauptprogramm: In der Wochenschau wird im März 1917 für Kriegsanleihen geworben",
+    "associationText1": "3 Jahre zuvor: Dampfern im Schwarzen Meer",
+    "target1": "3",
+    "associationText2": "Teaser der zweiten Assoziation",
+    "text": "Der Ertrag der VI. deutschen Kriegsanleihe gibt unseren Helden die Kraft zum Siege\n1917 Animationsfilm\nDer für die Messter-Wochenschau produzierte Zeichentrickfilm wirbt für die sechste, im März 1917 ausgegebene deutsche Kriegsanleihe. Wöchentlich wurden neue Zusammenstellung von Filmberichten über aktuelle Themen produziert. In diesem Fall gleichzeitig: Werbung."
+
+  },
+      {
+    "id": 3,
+    "section": "Schäden nach Beginn des Seekriegs im Schwarzen Meer",
+    "thumbnail": "ToDo - Thumbnail auswählen + bereitstellen",
+    "video": "https://www.filmportal.de/sites/default/files/video/DIF_50_145_1R_Kino_Kriegsschau_Nr_14_DE_1914.mp4",
+    "start": "2:01",
+    "end": "4:13",
+    "intro": "Kurz nach Kriegsbeginn sind in einer russischen Hafenstadt die Schäden eines türkischen Angriffs im Oktober 1914 bereits verheerend. In deutschen Kinos dokumentiert die \"Kino-Kriegsschau\".",
+    "associationText1": "Teaser der ersten Assoziation",
+    "target1": "",
+    "associationText2": "Teaser der zweiten Assoziation",
+    "text": "Aus: Kino-Kriegsschau Nr.14\n1914 Dokumentarfilm\nDer verheerende Angriff wurde zeitgleich mit Attacken auf Sewastopol und Odessa durchgeführt und markierte den Beginn des Seekriegs im Schwarzen Meer.\nMit drei Monaten Verzug vertreibt Filmproduzent Edmund Hubert aus Berlin diese Aufnahmen im Januar 1915 als Teil seiner Reihe \"Kino-Kriegsschau\" und bewarb sie als Sensation."
   }
 ],
 "arcs":[
   {
       "source": "European War, 1914-1915. The African Zouaves in the Flanders",
       "target": "TODO"
+  },
+  {
+      "source": "Der Ertrag der VI. deutschen Kriegsanleihe gibt unseren Helden die Kraft zum Siege",
+      "target": "Schäden nach Beginn des Seekriegs im Schwarzen Meer"
   }
   
 ]
