6fd6184a60f083f5da71b68f9606959e1b4d1e82
Gerd Müller <g.mueller@ewerk.com>
2018-11-20 15:24:13 +0100

    Modify chart dimension

--- index.js
+++ index.js
@@ -1,87 +1,97 @@
-var width = window.innerWidth,
-height = window.innerHeight;
+var width = 960,
+  height = 500;
 var color = d3.scale.category20();
 
 var force = d3.layout.force()
-    .size([width, height])
-    .linkDistance(width/3.05)
-    .charge(-500)
-    .start();
+  .size([width, height])
+  .linkDistance(100)
+  .charge(-2000)
+  .start();
 
 var svg = d3.select("#main-content").append("svg")
-            .attr("width", width)
-            .attr("height", height);   
+.attr("width", width)
+.attr("height", height)
 
-d3.json("films.json", function(error, graph){ 
-   
-var nodes = {};
-var links = graph.arcs;
-  
-// Compute the distinct nodes from the links.
-links.forEach(function(link) {
-  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-  
-});
+d3.json("films.json", function (error, graph) {
+
+  var nodes = {};
+  var links = graph.arcs;
+
+  // Compute the distinct nodes from the links.
+  links.forEach(function (link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {
+      name: link.source
+    });
+    link.target = nodes[link.target] || (nodes[link.target] = {
+      name: link.target
+    });
+
+  });
   force
-      .nodes(d3.values(nodes))
-      .links(links)
-      .on("tick", tick)
-      .start();
+    .nodes(d3.values(nodes))
+    .links(links)
+    .on("tick", tick)
+    .start();
 
   var drag = force.drag().on("dragstart", dragstart);
 
   svg.append("defs").selectAll("marker")
     .data(0)
-  .enter().append("marker")
-    .attr("id", function(d) { return d; })
+    .enter().append("marker")
+    .attr("id", function (d) {
+      return d;
+    })
     .attr("viewBox", "0 -5 10 10")
     .attr("refX", 25)
     .attr("refY", -1.5)
     .attr("markerWidth", 6)
     .attr("markerHeight", 6)
     .attr("orient", "auto")
-  .append("path")
+    .append("path")
     .attr("d", "M0,-5L10,0L0,5");
 
-var path = svg.append("g").selectAll("path")
+  var path = svg.append("g").selectAll("path")
     .data(force.links())
-  .enter().append("path")
-    .attr("class", function(d) { return "link " + 0; });
+    .enter().append("path")
+    .attr("class", function (d) {
+      return "link " + 0;
+    });
 
-var circle = svg.append("g").selectAll("circle")
+  var circle = svg.append("g").selectAll("circle")
     .data(force.nodes())
-  .enter().append("circle")
+    .enter().append("circle")
     .attr("r", 25)
-    .call(force.drag); 
+    .call(force.drag);
 
-var text = svg.append("g").selectAll("text")
+  var text = svg.append("g").selectAll("text")
     .data(force.nodes())
-  .enter().append("text")
+    .enter().append("text")
     .attr("x", 25)
     .attr("y", ".31em")
-    .text(function(d) { return d.name; });
+    .text(function (d) {
+      return d.name;
+    });
 
-// Use elliptical arc path segments to doubly-encode directionality.
-function tick() {
-  path.attr("d", linkArc);
-  circle.attr("transform", transform);
-  text.attr("transform", transform);
-}
+  // Use elliptical arc path segments to doubly-encode directionality.
+  function tick() {
+    path.attr("d", linkArc);
+    circle.attr("transform", transform);
+    text.attr("transform", transform);
+  }
 
-function linkArc(d) {
-  var dx = d.target.x - d.source.x,
+  function linkArc(d) {
+    var dx = d.target.x - d.source.x,
       dy = d.target.y - d.source.y,
       dr = Math.sqrt(dx * dx + dy * dy);
-  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-}
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  }
 
-function transform(d) {
-  return "translate(" + d.x + "," + d.y + ")";
-}
+  function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  }
 
-function dragstart(d) {
-  d3.select(this).classed("fixed", d.fixed= true);
-}
+  function dragstart(d) {
+    d3.select(this).classed("fixed", d.fixed = true);
+  }
 
 });
